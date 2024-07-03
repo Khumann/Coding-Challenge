f652565667fdc7f23904eaaa5f6cb2a0d0feb72e
Shubham <chaudhary.shubham812@gmail.com>
2024-07-03 13:58:01 +0100

    uploaded py file

---
+++ alacrity.py
@@ -0,0 +1,65 @@
+def prime(num):
+        if num == 1 or (num % 2 == 0 and num != 2):
+            return False
+        i = 3
+        while i < num:
+            if num % i == 0:
+                return False
+            i += 2
+        return True
+
+
+
+def get_factor(num):
+    factors = []
+    i = 2
+    while i < num:
+        if num % i == 0:
+            factors.append(i)
+        i += 1
+    return factors
+
+
+def check_prime(num):
+    if prime(num):
+        print("prime")
+    else:
+        print("Number is composite and its factors are:", get_factor(num))
+
+
+def gen_prime(low, high):
+    print("Function To be implemented")
+    pass
+
+
+if "__main__" == __name__:
+    while True:
+        print("Enter your Choice: ")
+        print(" For Prime: 1")
+        print(" For Generate in range: 2")
+        choice = int(input("Enter your choice: "))
+
+        if choice == 1:
+            number = int(input("Enter number to check: "))
+            check_prime(number)
+        elif choice == 2:
+            lower = int(input("Enter lower bound: "))
+            upper = int(input("Enter upper bound: "))
+            gen_prime(lower, upper)
+        else:
+            print("invalid input")
+            
+        
+        choice = input("Do you want to exit (Y/N): ")
+        if choice == "Y" or choice == "y":
+            break
+        elif choice == "N" or choice == "n":
+            continue
+        else:
+            print("invalid input")
+
+
+
+
+
+
